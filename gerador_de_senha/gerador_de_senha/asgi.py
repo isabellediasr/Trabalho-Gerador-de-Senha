a46f151c3e49586796c6c64f09a067ac46dad07e
LucasOliveira42 <lucasbogard3@gmail.com>
2024-10-04 19:50:18 -0300

    começo do projeto em django

---
+++ gerador_de_senha/gerador_de_senha/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gerador_de_senha project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerador_de_senha.settings')
+
+application = get_asgi_application()
