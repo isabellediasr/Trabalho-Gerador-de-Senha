3efd9e168dbf52186fe3456bb1bd1dbfcdb2c609
LucasOliveira42 <lucasbogard3@gmail.com>
2024-10-04 22:50:01 -0300

    começo  da implementação do backend

--- gerador_de_senha/gerador/models.py
+++ gerador_de_senha/gerador/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Cadeado(models.Model):
+    site = models.CharField(max_length=8000)
+    login = models.CharField(max_length=255)
+    senha = models.CharField(max_length=255)
--- gerador_de_senha/gerador/views.py
+++ gerador_de_senha/gerador/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import random
+import string
 
 def gerador(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(16))
+
+    response = {"senha" : senha,}
+
+
+    return HttpResponse(template.render(response, request))
 
 # Create your views here.
