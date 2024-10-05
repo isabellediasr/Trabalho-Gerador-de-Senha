4649b6d1769ce0fe56da41962ce9e449162684ae
LucasOliveira42 <lucasbogard3@gmail.com>
2024-10-04 21:37:23 -0300

    pasta de templastes criado para o frontend e app listado nos settings

--- gerador_de_senha/gerador/views.py
+++ gerador_de_senha/gerador/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def gerador(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
 
 # Create your views here.
